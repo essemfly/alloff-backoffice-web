a092c87a1554b7d792af1686159ddb11db18deef
Stephen Cheong <j.stephen.cheong@gmail.com>
2022-02-03 01:56:05 +0900

    After merging care --- reconciled some import errors

--- src/helpers/timedeal.ts
+++ src/helpers/timedeal.ts
@@ -1,26 +1,28 @@
-export type TimedealDomainStatus = "PENDING" | "OPEN" | "CLOSED";
-function getTimedealStatus(timedeal: Timedeal): TimedealDomainStatus;
-function getTimedealStatus(
-  starttime: Date,
-  finishtime: Date,
-): TimedealDomainStatus;
-function getTimedealStatus(
-  timedealOrStarttime: Timedeal | Date,
-  finishTime?: Date,
-): TimedealDomainStatus {
-  if (timedealOrStarttime instanceof Date && finishTime === undefined) {
-    throw new Error("starttime must accompany finishtime!");
-  }
+// export type TimedealDomainStatus = "PENDING" | "OPEN" | "CLOSED";
+// function getTimedealStatus(timedeal: Timedeal): TimedealDomainStatus;
+// function getTimedealStatus(
+//   starttime: Date,
+//   finishtime: Date,
+// ): TimedealDomainStatus;
+// function getTimedealStatus(
+//   timedealOrStarttime: Timedeal | Date,
+//   finishTime?: Date,
+// ): TimedealDomainStatus {
+//   if (timedealOrStarttime instanceof Date && finishTime === undefined) {
+//     throw new Error("starttime must accompany finishtime!");
+//   }
 
-const getTimedealStatusColor = (status: TimedealDomainStatus) => {
-  switch (status) {
-    case "CLOSED":
-      return "gray";
-    case "PENDING":
-      return "teal";
-    case "OPEN":
-      return "green";
-  }
-};
+// const getTimedealStatusColor = (status: TimedealDomainStatus) => {
+//   switch (status) {
+//     case "CLOSED":
+//       return "gray";
+//     case "PENDING":
+//       return "teal";
+//     case "OPEN":
+//       return "green";
+//   }
+// };
 
-export { getTimedealStatus, getTimedealStatusColor };
+// export { getTimedealStatus, getTimedealStatusColor };
+
+export {};
