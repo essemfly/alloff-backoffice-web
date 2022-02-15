a0af48d2e52be166a9db283c04ac9fbed3f64d33
ClareKang <shiningkey89@gmail.com>
2022-02-15 18:14:11 +0900

    feat: add data table component w generic

--- src/components/DataTable/DataTable.svelte
+++ src/components/DataTable/DataTable.svelte
@@ -1,46 +1,80 @@
 <script lang="ts">
+  import { Button, DataTable } from "carbon-components-svelte";
+  import Launch16 from "carbon-icons-svelte/lib/Launch16";
   import { createEventDispatcher } from "svelte";
-  import { DataTable } from "carbon-components-svelte";
-  import type {
-    DataTableHeader,
-    DataTableRow,
-    DataTableCell,
-  } from "carbon-components-svelte/types/DataTable/DataTable";
 
-  export type T = any;
+  import {
+    DataTableColumn,
+    DataTableData,
+    getHeaders,
+    getRows,
+  } from "./helpers";
 
-  type TableHeader<T> = DataTableHeader & Record<keyof T, any>;
-  type TableRow<T> = DataTableRow & Record<keyof T, any>;
+  type T = any;
 
-  export let headers: TableHeader<T>[];
-  export let rows: TableRow<T>[];
-  export let sortable: boolean = false;
+  export let columns: DataTableColumn<T>[] = [];
+  export let data: DataTableData<T>[] = [];
 
   const dispatch = createEventDispatcher();
 
-  const handleRowClick = (event: CustomEvent<DataTableRow>) => {
-    dispatch("click:row", event);
+  let rows = getRows<T>(data, columns);
+  const headers = getHeaders<T>(columns);
+  const imageColumns = columns
+    .filter(({ type }) => type === "image")
+    .map(({ key }) => key);
+  const linkColumns = columns
+    .filter(({ type }) => type === "link")
+    .map(({ key }) => key);
+
+  const handleOpen = (path: string) => () => {
+    window.open(path, "_blank"); // todo: not use window.open
   };
 
-  const handleCellClick = (event: CustomEvent<DataTableCell>) => {
-    dispatch("click:cell", event);
-  };
+  $: if (data) {
+    rows = getRows<T>(data, columns);
+  }
 </script>
 
-<DataTable
-  {headers}
-  {rows}
-  on:click:row={handleRowClick}
-  on:click:cell={handleCellClick}
-  {sortable}
->
-  <span slot="cell" let:cell>
-    {cell.value}
+<DataTable {headers} {rows}>
+  <span
+    slot="cell"
+    let:cell
+    let:row
+    on:click={() => {
+      dispatch("click", { row, cell });
+      dispatch("click:cell", cell);
+    }}
+  >
+    {#if imageColumns.includes(cell.key) && cell.value}
+      <img
+        src={cell.value}
+        alt={[row.id, "thumbnail"].join("-")}
+        class="cell_thumb"
+      />
+    {:else if linkColumns.includes(cell.key) && cell.value}
+      <Button
+        tooltipPosition="bottom"
+        tooltipAlignment="end"
+        iconDescription="상세"
+        icon={Launch16}
+        kind="ghost"
+        size="small"
+        on:click={handleOpen(cell.value)}
+      />
+    {:else}
+      {cell.value}
+    {/if}
   </span>
 </DataTable>
 
 <style>
   :global(.bx--data-table tr) {
     cursor: pointer;
+  }
+
+  .cell_thumb {
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
   }
 </style>
