e07bdf8213ca6187b61aafda411dfb74412aea23
ClareKang <shiningkey89@gmail.com>
2022-03-23 13:45:49 +0900

    feat: add image upload field

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,6 +1,7 @@
 export { default as AutocompleteField } from "./AutocompleteField.svelte";
 export { default as BrandSelectField } from "./BrandSelectField.svelte";
 export { default as CategorySelectField } from "./CategorySelectField.svelte";
+export { default as ImageUploadField } from "./ImageUploadField.svelte";
 export { default as RadioField } from "./RadioField.svelte";
 export { default as TextField } from "./TextField.svelte";
 export { default as ToggleField } from "./ToggleField.svelte";
