6616b05374a0249a9840433379f4028c52e13b72
ClareKang <shiningkey89@gmail.com>
2022-03-29 23:53:05 +0900

    feat: add validation rules to product group form

--- src/pages/product-groups/views/ProductGroupAdd.svelte
+++ src/pages/product-groups/views/ProductGroupAdd.svelte
@@ -1,64 +1,71 @@
 <script lang="ts">
   import { DateTime } from "luxon";
   import { toast } from "@zerodevx/svelte-toast";
+  import { onMount } from "svelte";
   import { navigate } from "svelte-navigator";
   import { Grid, Button } from "carbon-components-svelte";
   import Save16 from "carbon-icons-svelte/lib/Save16";
 
-  import { ProductGroup, ProductGroupsApi, GroupTypeEnum } from "@api";
+  import { ProductGroupsApi, GroupTypeEnum, ProductInGroup } from "@api";
   import Nav from "@app/components/Nav.svelte";
+  import { convertToSnakeCase } from "@app/helpers/change-case";
 
   import ProductForm from "./components/ProductGroupForm.svelte";
   import { getGroupTypeLabel } from "../commands/helpers";
+  import { formStore } from "../models/schema";
 
   export let productGroupType: GroupTypeEnum;
 
-  let isTouched = true;
+  let isSubmitting = false;
   let productGroupTypeLabel = getGroupTypeLabel(productGroupType);
+  let productInGroups: ProductInGroup[] = [];
 
-  let productGroup: ProductGroup = {
-    title: "",
-    short_title: "",
-    image_url: "",
-    start_time: "",
-    finish_time: "",
-    product_group_id: "",
-    products: [],
-    instruction: [],
-    group_type: productGroupType,
-  };
+  const productGroupApi = new ProductGroupsApi();
 
-  const handleSubmit = async () => {
+  onMount(() => {
+    formStore.update({ groupType: productGroupType });
+  });
+
+  const handleSubmit = async (event: MouseEvent) => {
+    isSubmitting = true;
     try {
-      const productGroupApi = new ProductGroupsApi();
-      const { products, ...requestBody } = productGroup;
-
+      event.preventDefault();
+      const formData = {
+        ...$formStore.fields,
+        ...(productGroupType !== GroupTypeEnum.Timedeal && {
+          startTime: DateTime.now().toISO(),
+          finishTime: DateTime.now().toISO(),
+        }),
+      };
+      const isValid = await formStore.validate(formData, {
+        context: { isAdding: true },
+      });
+      if (!isValid) {
+        toast.push("일부 항목값이 올바르지 않습니다.");
+        return;
+      }
       const res = await productGroupApi.productGroupsCreate({
-        createProductGroupSeriazlierRequest: {
-          ...requestBody,
-          ...(productGroup.group_type !== GroupTypeEnum.Timedeal && {
-            start_time: DateTime.now().toISO(),
-            finish_time: DateTime.now().toISO(),
-          }),
-        },
+        createProductGroupSeriazlierRequest: convertToSnakeCase(formData),
       });
-
-      const newProductGroup = res.data;
-
-      await productGroupApi.productGroupsPushProductsCreate({
-        id: newProductGroup.product_group_id,
-        pushProductsRequest: {
-          product_group_id: newProductGroup.product_group_id,
-          product_priority: products.map(({ product, priority }) => ({
-            product_id: product.alloff_product_id,
-            priority,
-          })),
-        },
-      });
+      if (productInGroups.length > 0) {
+        const newProductGroup = res.data;
+        await productGroupApi.productGroupsPushProductsCreate({
+          id: newProductGroup.product_group_id,
+          pushProductsRequest: {
+            product_group_id: newProductGroup.product_group_id,
+            product_priority: productInGroups.map(({ product, priority }) => ({
+              product_id: product.alloff_product_id,
+              priority,
+            })),
+          },
+        });
+      }
       toast.push(`${productGroupTypeLabel} 등록이 완료되었습니다.`);
       navigate(-1);
     } catch (e) {
       toast.push(`${productGroupTypeLabel} 등록에 오류가 발생했습니다.`);
+    } finally {
+      isSubmitting = false;
     }
   };
 </script>
@@ -66,18 +73,16 @@
 <Nav title="{productGroupTypeLabel} 추가">
   <Grid>
     <div class="button-right-wrapper mb10">
-      <Button on:click={handleSubmit} disabled={!isTouched} icon={Save16}>
-        {productGroupTypeLabel} 등록
+      <Button on:click={handleSubmit} disabled={isSubmitting} icon={Save16}>
+        {productGroupTypeLabel}
+        {isSubmitting ? "등록중..." : "등록"}
       </Button>
     </div>
-    <ProductForm
-      label={productGroupTypeLabel}
-      bind:form={productGroup}
-      isAdding
-    />
+    <ProductForm label={productGroupTypeLabel} isAdding bind:productInGroups />
     <div class="button-right-wrapper mt10">
-      <Button on:click={handleSubmit} disabled={!isTouched} icon={Save16}>
-        {productGroupTypeLabel} 등록
+      <Button on:click={handleSubmit} disabled={isSubmitting} icon={Save16}>
+        {productGroupTypeLabel}
+        {isSubmitting ? "등록중..." : "등록"}
       </Button>
     </div>
   </Grid>
