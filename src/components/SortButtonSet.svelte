17fef7d070160e0dea42bef6fa64b1c2f498d7f6
ClareKang <shiningkey89@gmail.com>
2022-03-31 13:49:41 +0900

    feat: add sort button set to components

---
+++ src/components/SortButtonSet.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { Button } from "carbon-components-svelte";
+  import ChevronUp16 from "carbon-icons-svelte/lib/ChevronUp16";
+  import ChevronDown16 from "carbon-icons-svelte/lib/ChevronDown16";
+  import UpToTop16 from "carbon-icons-svelte/lib/UpToTop16";
+  import DownToBottom16 from "carbon-icons-svelte/lib/DownToBottom16";
+
+  export let value: any[] = [];
+  export let index: number = -1;
+  export let disabled: boolean = false;
+
+  const dispatch = createEventDispatcher();
+
+  const handleSort = (to: number) => () => {
+    const [current] = value.splice(index, 1);
+    switch (to) {
+      case 0:
+        // to first
+        value = [current, ...value];
+        break;
+      case 100:
+        // to last
+        value = [...value, current];
+        break;
+      default:
+        const toIndex = index + to;
+        value.splice(toIndex, 0, current);
+        value = value;
+    }
+    dispatch("change", value);
+  };
+</script>
+
+<Button
+  tooltipPosition="bottom"
+  tooltipAlignment="end"
+  iconDescription="첫번째로"
+  icon={UpToTop16}
+  kind="ghost"
+  size="small"
+  on:click={handleSort(0)}
+  disabled={index === 0 || disabled}
+/>
+<Button
+  tooltipPosition="bottom"
+  tooltipAlignment="end"
+  iconDescription="위로"
+  icon={ChevronUp16}
+  kind="ghost"
+  size="small"
+  on:click={handleSort(-1)}
+  disabled={index === 0 || disabled}
+/>
+<Button
+  tooltipPosition="bottom"
+  tooltipAlignment="end"
+  iconDescription="아래로"
+  icon={ChevronDown16}
+  kind="ghost"
+  size="small"
+  on:click={handleSort(+1)}
+  disabled={index === value.length - 1 || disabled}
+/>
+<Button
+  tooltipPosition="bottom"
+  tooltipAlignment="end"
+  iconDescription="마지막으로"
+  icon={DownToBottom16}
+  kind="ghost"
+  size="small"
+  on:click={handleSort(+100)}
+  disabled={index === value.length - 1 || disabled}
+/>
